c30742959fab6534eda62f195d84175de3b184f8
re0ah <re0ah@yandex.ru>
2020-07-10 14:13:36 +0300

    made the count of argc for each cmd and num of separators. Then need I do to parser & execs for '|', '||', '>', '>>'. I already thought about how do it, but i don't know how to do '&'. An alghorithm has already been made on C and left only rewriting it on assembly.

--- main.asm
+++ main.asm
@@ -23,6 +23,8 @@
 ;
 ;For more information, please refer to <http://unlicense.org/>
 
+;thanks https://chromium.googlesource.com/chromiumos/docs/+/master/constants/syscalls.md for comfortable table of syscalls
+
 bits 64
 
 %define PAGESIZE 4096
@@ -170,9 +172,7 @@ skip_spaces:
 delete_space_holes:
 		sub		rsp, 	READ_BUFFER_MAX_SIZE ;alloc stack memory
 		
-;		xor		rdx,	rdx 	;bool for check whether we are in brackets
-								;in general, zeroing it makes no sense,
-						    ;so I commented this
+		xor		rdx,	rdx 	;bool for check whether we are in brackets
 
 		xor		rax, 	rax 	;iterator for rcx_buffer
 		xor		rbp,	rbp 	;iterator for stack buffer
@@ -183,10 +183,10 @@ delete_space_holes:
 		cmp		rdx,	1
 		je		.dsh_if_in_double_quote_now
 		cmp		bl,		'"'
-		jne		.dsh_if_i_not_double_quote
+		jne		.dsh_if_in_not_double_quote
 		mov		rdx,	1
 		jmp		.dsh_loop_epilogue
-.dsh_if_i_not_double_quote: 
+.dsh_if_in_not_double_quote: 
 		cmp		bx,		0x2020		;'  ', two spaces in a row
 		jne		.dsh_loop_epilogue
 		inc		rax
@@ -224,12 +224,73 @@ delete_space_holes:
 ;----------------------------------------------------------------
 ;args:
 ;	1. rcx, ptr to buffer
-;Function skip all spaces and return pointer after spaces
 ;
-;modify registers: rcx = rcx + n, n - num of spaces
-;				   rax = 0x20, uncomment push/pop/sub for get num of spaces
+;replace some sequences to single char
+	;'\ ' = 0x01
+	;'\"' = 0x02
+	;'&&' = 0x03
+	;'||' = 0x04
+	;'>>' = 0x05
+;
+;modify registers: rdx = new size of rcx_buffer
+				  ;rax = prev size of rcx_buffer
+				  ;rbx = rcx_buffer[1]
+				  ;rbp = 0
 replace_special_symbols:
-		sub		rsp,	READ_BUFFER_MAX_SIZE ;allocate stack memory
+		sub		rsp, 	READ_BUFFER_MAX_SIZE ;alloc stack memory
+		
+		xor		rax, 	rax 	;iterator for rcx_buffer
+		xor		rbp,	rbp 	;iterator for stack buffer
+	;copy on stack buffer from rcx according by the rules on the comment
+;of this function
+.rsp_loop:
+		mov		bx,  	word[rcx + rax]
+		cmp		bx,		0x5C20		;'\ '
+		je		.l0
+		cmp		bx,		0x5C22		;'\"'
+		je		.l1
+		cmp		bx,		0x2626		;'&&'
+		je		.l2
+		cmp		bx,		0x7C7C		;'||'
+		je		.l3
+		cmp		bx,		0x3E3E		;'>>'
+		je		.l4
+		jmp		.rsp_l
+.l0:	
+		mov		bl,		0x01
+		jmp		.rsp_l
+.l1:	
+		mov		bl,		0x02
+		jmp		.rsp_l
+.l2:	
+		mov		bl,		0x03
+		jmp		.rsp_l
+.l3:	
+		mov		bl,		0x04
+		jmp		.rsp_l
+.l4:	
+		mov		bl,		0x05
+		jmp		.rsp_l
+.rsp_l:
+.rsp_l:
+		mov		byte [rsp + rbp], bl
+		inc		rax
+		inc 	rbp
+		test	bl,		bl
+		jne		.rsp_loop
+
+		dec		rbp		;the cycle is written so that an extra 1 added to rbp
+	;in rbp at the moment stores size of stack_buffer. Perhaps it will
+;still be needed - save it in rdx
+		mov		rdx,	rbp
+
+	;copy on rcx_buffer from stack.
+	;This cycle don't copy first element, but I don't need
+.rsp_cp_buf:
+		mov		bl,		byte[rsp + rbp]
+		mov		byte[rcx + rbp], bl
+		dec		rbp
+		jnz		.rsp_cp_buf
 
 		add		rsp,	READ_BUFFER_MAX_SIZE ;free stack memory
 		ret
@@ -244,6 +305,22 @@ replace_special_symbols:
 ;				   rax = 0x20, uncomment push/pop/sub for get num of spaces
 sep_str_to_cmd:
 		
+		ret
+;----------------------------------------------------------------
+
+;----------------------------------------------------------------
+;args:
+;	1.	rsi,	argv
+;modify registers: rax = 0
+;				   r12 = argc
+calc_argc:
+		xor		rax,	rax
+.e_count_argc_loop:
+		cmp		[rsi + r12 * 8], rax
+		je		.e_count_argc_loop_end
+		inc		r12
+		jmp		.e_count_argc_loop
+.e_count_argc_loop_end:
 		ret
 ;----------------------------------------------------------------
 
@@ -310,13 +387,7 @@ execve_as_script:
 execvp:
 	;counting argc
 		xor		r12,	r12
-		xor		rax,	rax
-.e_count_argc_loop:
-		cmp		[rsi + r12 * 8], rax
-		je		.e_count_argc_loop_end
-		inc		r12
-		jmp		.e_count_argc_loop
-.e_count_argc_loop_end:
+		call	calc_argc
 	;check if pathname contain '/'
 		mov		rbp,	[rsi]
 .e_check_slash: 
@@ -355,8 +426,7 @@ execvp:
 		;char *const envp[]); 
 		push	rax
 		push	rsi
-		mov		rdi,	rsp
-		add		rdi,	16
+		lea		rdi,	[rsp + 16]
 		mov		rsi,	rsi
 		xor		rdx,	rdx
 		mov		rax,	0x3b	;execve
@@ -404,9 +474,7 @@ parse_cmd:
 .lp:	cmp		byte[rcx + rax],	dil
 		jne		.ne2
 		mov		byte[rcx + rax],	bl
-		mov		r9,		rcx
-		add		r9,		rax
-		inc		r9
+		lea		r9,		[rcx + rax + 1]
 		mov		qword[rsp + 8 * rsi],	r9
 		inc		rsi
 .ne2:	inc		rax
@@ -418,14 +486,126 @@ parse_cmd:
 		mov		rbx,	2
 		call 	execvp
 
-;int execve(const char *filename, char *const argv[],
-;char *const envp[]); 
-;		mov		rdi,	[rsp]
-;		mov		rsi,	rsp
-;		mov		rdx,	0
-;		mov		rax,	0x3b	;execve
-;		syscall
 		add		rsp,	32
+		ret
+;----------------------------------------------------------------
+
+;----------------------------------------------------------------
+;args:
+;	1.	bl,	char
+;list of seps:
+	;'|'
+	;'&'
+	;'>'
+	;';'
+	;'&&' = 0x03
+	;'||' = 0x04
+	;'>>' = 0x05
+;modify registers: rbx - 1 or 0
+if_sep:
+		cmp		bl,	'|'
+		je		.is_true
+		cmp		bl,	'&'
+		je		.is_true
+		cmp		bl,	'>'
+		je		.is_true
+		cmp		bl,	';'
+		je		.is_true
+		cmp		bl,	0x03	;'&&'
+		je		.is_true
+		cmp		bl,	0x04	;'||'
+		je		.is_true
+		cmp		bl,	0x05	;'>>'
+		je		.is_true
+		xor		rbx,	rbx
+		ret
+.is_true:
+		mov		rbx,	1
+		ret
+;----------------------------------------------------------------
+;args:
+;	1.	rcx,	buf
+;modify registers: rax = size of buffer + 1
+				  ;r14 = num of sep
+				  ;bl = 0
+				  ;r11 = 0 or 1
+
+calc_sep:
+		xor		rax,	rax
+		xor		r14,	r14
+.cs_loop:
+		call	if_sep
+		test	rbx,	rbx
+		je		.cs_1
+		inc		r14
+.cs_1	inc		rax
+		mov		bl,	byte[rcx + rax]
+		test	bl,	bl
+		jne		.cs_loop
+
+		ret
+;----------------------------------------------------------------
+
+;----------------------------------------------------------------
+;get argc* and calc argc for each cmd
+;fill sep*
+;
+;args:
+;	1.	rcx,	buf
+;	2.	rsi,	argc*
+;	3.	rdi,	sep*
+;modify registers: rax = size of buffer + 1
+				  ;r14 = num of sep
+
+fill_argc:
+	;in this function we made almost the same in delete_space_holes.
+;maybe I should combine them? Later. Maybe.
+	;this code is bad, but is correct. Rewrite this later.
+		xor		rdx,	rdx 	;bool for check whether we are in brackets
+
+		xor		rax, 	rax 	;iterator for rcx_buffer
+		xor		rbp,	rbp		;iterator for argc
+.fa_loop2:
+		mov		r10,	2		;counter of single argc
+.fa_loop:
+		mov		bl,  	byte[rcx + rax]
+		cmp		rdx,	1
+		je		.fa_if_in_double_quote_now
+		cmp		bl,		'"'
+		jne		.fa_if_in_not_double_quote
+		mov		rdx,	1
+		jmp		.fa_loop_epilogue
+.fa_if_in_not_double_quote: 
+		cmp		bl,		0x20 ;' '
+		jne		.fa_check_sep
+		inc		rax
+		inc		r10
+		jmp		.fa_loop
+.fa_check_sep:
+		call	if_sep
+		test	rbx,	rbx
+		je		.fa_loop_pre
+		mov		dword[rsi + (rbp * 4)], r10d
+		mov		bl,  	byte[rcx + rax]
+		mov		byte[rdi + rbp], bl
+		inc		rax
+		inc		rbp
+		jmp		.fa_loop2
+.fa_loop_pre:
+		inc		rax
+		mov		bl,  	byte[rcx + rax - 1]
+		test	bl,		bl
+		jne		.fa_loop
+.fa_if_in_double_quote_now:
+		cmp		bl,		'"'
+		jne		.fa_loop_epilogue
+		xor		rdx,	rdx
+.fa_loop_epilogue:
+		inc		rax
+		test	bl,		bl
+		jne		.fa_loop
+		mov		dword[rsi + (rbp * 4)], r10d
+
 		ret
 ;----------------------------------------------------------------
 
@@ -443,13 +623,40 @@ _start:
 		mov		rdx,	rax		;in rax size of input bytes
 		mov		rcx,	read_buffer_data
 		call 	skip_spaces
+		call	replace_special_symbols
 		call 	delete_space_holes
-;		call	replace_special_symbols
-;		call	sep_str_to_cmd
-		call	parse_cmd
+		call	calc_sep
+		;num of sep = r14, num of cmd = r14 + 1.
+		;what is cmd? Argv & argc. We can know argc for each cmd with
+	;help simple func, and after alloc the right amount of memory
+	;argc = 4 byte. Need alloc (r14 + 1) * 4 bytes for argc*
+	;sep  = 1 byte. Need alloc r14 bytes for sep*
+		lea		r15,	[(r14 + 1) * 4]
+		add		r15,	r14
+		sub		rsp,	r15
+;		push	r15
+		;register states on this step:
+			;rcx = ptr to buf
+			;rdx = size of buf
+			;rax = size of buf
+			;r14 = num of separators
+			;r15 = size of allocate to argc*
+			;[rsp..rsp+r14] = memory of sep
+			;[rsp+r14..rsp+r15] = memory of argc
+			;bl = 0
+			;rbp = 0
+			;rdi = 1
+		mov		rdi,	rsp
+		lea		rsi,	[rsp + r14]
+		call	fill_argc
+		;call	sep_str_to_cmd
+		;call	parse_cmd
+;		pop		r15
+		add		rsp,	r15
 
 		xor		rdi,	rdi		;stdout
 		mov		rsi,	rcx
+		mov		rdx,	24
 		mov		rax,	1		;write
 		syscall
 	;Terminate program
